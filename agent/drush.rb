5352e0c08b8934da8c91e7c869caa17f439e4556
Julien Pivotto <roidelapluie@inuits.eu>
2013-07-23 13:55:32 +0200

    use the native run() function of mcollective

--- agent/drush.rb
+++ agent/drush.rb
@@ -2,7 +2,8 @@ module MCollective
   module Agent
     class Drush<RPC::Agent
       action "cache-clear" do
-        reply[:exitcode], reply[:stdout], reply[:stderr], reply[:command] = Drush.run("cache clear #{request[type]}", request)
+        reply[:command] = Drush.get_drush_command("cache clear #{request[type]}", request)
+        reply[:exitcode] = run(reply[:command], :stdout => :stdout, :stderr => :stderr)
         if reply[:exitcode] == 0 then
           reply.statusmsg = "OK"
         else
@@ -10,14 +11,15 @@ module MCollective
         end
       end
       action "updatedb" do
-        reply[:exitcode], reply[:stdout], reply[:stderr], reply[:command] = Drush.run("updatedb", request)
+        reply[:command] = Drush.get_drush_command("updatedb", request)
+        reply[:exitcode] = run(reply[:command], :stdout => :stdout, :stderr => :stderr)
         if reply[:exitcode] == 0 then
           reply.statusmsg = "OK"
         else
           reply.fail "FAIL"
         end
       end
-      def self.run(drush_command, request)
+      def self.get_drush_command(drush_command, request)
         command = "/usr/bin/drush"
         if request[:yes] then
           command += " --yes"
@@ -29,16 +31,9 @@ module MCollective
           command += " --uri=#{request[:uri]}" 
         end
         command += " --root=#{request[:root]}"
-
         command += " #{drush_command}"
 
-        output = ""
-        output_errors = ""
-        shell = Shell.new(command, {:stdout => output, :stderr => output_errors, :chomp => true})
-        shell.runcommand
-        exitcode = shell.status.exitstatus
-
-        return exitcode, output, output_errors, command
+        return command
       end
     end
   end
